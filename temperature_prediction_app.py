43d112a43dd97df2c5745beb00acc28d6b197b5e
alonge9500 <alongedaniel19@gmail.com>
2024-10-02 12:58:57 +0100

    completed

--- temperature_prediction_app.py
+++ temperature_prediction_app.py
@@ -9,10 +9,8 @@ import pickle
 from sklearn.model_selection import GridSearchCV
 
 # Load the trained model and scaler from the pickles folder
-model = joblib.load('pickles/final_randomforest_model.pkl')
-model2 = joblib.load('pickles/xgb.pkl')
-model3 = joblib.load('pickles/random_forest_model.pkl')
-model4 = joblib.load('pickles/random_forest_model_main.pkl')
+xgbmodel = joblib.load('pickles/xgb.pkl')
+random_forest_model = joblib.load('pickles/random_forest_model.pkl')
 scaler = joblib.load('pickles/scaler_minmax.pkl')
 
 # Function to scale the input features
@@ -22,19 +20,26 @@ def scale_features(features):
 # Define the Streamlit app
 def main():
     st.title("Temperature Prediction App")
+    
 
     # Create a form for user inputs
     with st.form(key='input_form'):
+        col1, col2, col3 = st.columns(3)
         # Create input fields for the features
-        month = st.selectbox("Month", list(range(1, 13)))  # January=1, December=12
-        cloud_cover = st.number_input("Cloud Cover (0-100)", min_value=0.0)
-        wind_speed = st.number_input("Wind Speed (m/s)", min_value=0.0)
-        wind_gust = st.number_input("Wind Gust (m/s)", min_value=0.0)
-        humidity = st.number_input("Humidity (%)", min_value=0.0)
-        pressure = st.number_input("Pressure (hPa)", min_value=0.0, format="%.4f")
-        global_radiation = st.number_input("Global Radiation (W/m²)", min_value=0.0)
-        precipitation = st.number_input("Precipitation (mm)", min_value=0.0)
-        sunshine = st.number_input("Sunshine (hours)", min_value=0.0)
+        with col1:
+            month = st.selectbox("Month", list(range(1, 13)))  # January=1, December=12
+            cloud_cover = st.number_input("Cloud Cover (0-100)", min_value=0.0)
+            wind_speed = st.number_input("Wind Speed (m/s)", min_value=0.0)
+        with col2:
+            wind_gust = st.number_input("Wind Gust (m/s)", min_value=0.0)
+            humidity = st.number_input("Humidity (%)", min_value=0.0)
+            pressure = st.number_input("Pressure (hPa)", min_value=0.0, format="%.4f")
+
+
+        with col3:
+            global_radiation = st.number_input("Global Radiation (W/m²)", min_value=0.0)
+            precipitation = st.number_input("Precipitation (mm)", min_value=0.0)
+            sunshine = st.number_input("Sunshine (hours)", min_value=0.0)
 
         # Submit button for the form
         submit_button = st.form_submit_button(label='Predict Temperature')
@@ -61,14 +66,10 @@ def main():
 
 
         # Make prediction
-        prediction = model.predict(scaled_features)
-        prediction2 = model2.predict(scaled_features)
-        prediction3 = model3.predict(scaled_features)
-        prediction4 = model4.predict(scaled_features)
-        st.success(f"The predicted temperature is: {prediction[0]:.2f} °C")
-        st.success(f"The predicted temperature isXGB: {prediction2[0]:.2f} °C")
-        st.success(f"The predicted temperature is RF: {prediction3[0]:.2f} °C")
-        st.success(f"The predicted temperature is RFMain: {prediction4[0]:.2f} °C")
+        xgbprediction = xgbmodel.predict(scaled_features)
+        rfprediction = random_forest_model.predict(scaled_features)
+        st.success(f"The predicted temperature (By XGBOOST MODEL) is: {xgbprediction[0]:.2f} °C")
+        st.success(f"The predicted temperature (BY RANDOM FOREST) is: {rfprediction[0]:.2f} °C")
 
 if __name__ == "__main__":
     main()
